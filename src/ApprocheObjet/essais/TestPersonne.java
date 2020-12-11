532c39dcb6152d392af57ea4c278886326a4a83c
Damien Martinez <damienca.martinez@gmail.com>
2020-12-11 12:11:17 +0100

    Ajout de methodes get et set dans TestPersonne

--- src/ApprocheObjet/essais/TestPersonne.java
+++ src/ApprocheObjet/essais/TestPersonne.java
@@ -14,6 +14,9 @@ public class TestPersonne {
         System.out.println(test1.prenom+" "+test1.nom+" vit a : "+test1.adresse.numeroRue+" "+test1.adresse.libelleRue+", "+test1.adresse.ville+", "+test1.adresse.codePostal);
         System.out.println(test2.prenom+" "+test2.nom+" vit a : "+test2.adresse.numeroRue+" "+test2.adresse.libelleRue+", "+test2.adresse.ville+", "+test2.adresse.codePostal);
 
-
+        test1.AfficherNomMajuscule();
+        System.out.println(test1.ObtenirNom());
+        test1.ModifierNom("Paul Narev");
+        System.out.println(test1.ObtenirNom());
     }
 }
